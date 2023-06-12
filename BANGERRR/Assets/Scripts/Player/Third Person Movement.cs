5bb38f1759a401d6982194557221ea93728122ee
Quentin4h04 <quentinhuet4500@gmail.com>
2023-06-12 15:41:42 +0200

    save before getting rid of the character controller

--- BANGERRR/Assets/Scripts/Player/Third Person Movement.cs
+++ BANGERRR/Assets/Scripts/Player/Third Person Movement.cs
@@ -7,59 +7,80 @@ public class ThirdPersonMovement : MonoBehaviour
     public CharacterController controller;
     public Transform cam;
 
-    // DÉPLACEMENT X - Y
-    public float speed = 6f;
+    public float _speed = 6f;
+    public float _turnSmoothTime = 0.1f;
+    float _turnSmoothVelocity;
 
-    public float turnSmoothTime = 0.1f;
-    float turnSmoothVelocity;
+    Vector3 _velocity;
+    public float _gravity = -9.81f;
+    public float _jumpHeight = 3f; // third person cam system
+    public float _jumpForce = 1500f; // gravitybodies system
 
-    // DÉPLACEMENT Y (avec la gravité et tout)
-    Vector3 velocity;
-    public float gravity = -9.81f;
-    public float jumpHeight = 3f;
+    public Transform _groundCheck;
+    public float _groundDistance = 0.4f;
+    public LayerMask _groundMask;
 
-    public Transform groundCheck;
-    public float groundDistance = 0.4f;
-    public LayerMask groundMask;
-    bool isGrounded;
+    bool _isGrounded;
 
-    // Update is called once per frame
+    private Rigidbody _rigidbody;
+    private Vector3 _direction;
+    private GravityBody _gravityBody;
+
+    void Start()
+    {
+        _rigidbody = transform.GetComponent<Rigidbody>();
+        _gravityBody = transform.GetComponent<GravityBody>();
+    }
+
     void Update()
     {
-        // DÉPLACEMENT Y (avec la gravité et tout)
-        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask); // check si touche le sol
-        if (isGrounded && velocity.y < 0)
-        {
-            velocity.y = -2f;
-        }
-
-
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
-        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
-        // normalized pour pas aller + vite si 2 touches sont activées
+        _direction = new Vector3(horizontal, 0f, vertical).normalized; // normalized pour pas aller + vite si 2 touches sont activées
 
-        // DÉPLACEMENT X - Y
-        if (direction.magnitude >= 0.01f) // test de si ça bouge (tuto : 0.1f)
+        // DÉPLACEMENT Y (avec la gravité et tout)
+        _isGrounded = Physics.CheckSphere(_groundCheck.position, _groundDistance, _groundMask); // check si touche le sol
+        if (_isGrounded && _velocity.y < 0)
         {
-            // On utilise Atan2 pour récupérer l'angle de direcrtion
-            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y; // angle calculé en fonction de la direction du joueur + la rotation de la cam
-            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime); // pour smooth l'angle
-            transform.rotation = Quaternion.Euler(0f, angle, 0f); // Appliquer la rotation au joueur
-
-            Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward; // mouvement prenant en compte la rotation de la cam
-            controller.Move(moveDirection.normalized * speed * Time.deltaTime);
+            _velocity.y = -2f;
         }
 
         // DÉPLACEMENT Y (avec la gravité et tout)
         // Saut
-        if (Input.GetButtonDown("Jump") && isGrounded)
+        if (Input.GetButtonDown("Jump") && _isGrounded)
         {
-            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+            _velocity.y = Mathf.Sqrt(_jumpHeight * -2f * _gravity);
+            //_rigidbody.AddForce(-_gravityBody.GravityDirection * _jumpForce, ForceMode.Impulse); // gravitybodies system
         }
 
         // Gravité
-        velocity.y += gravity * Time.deltaTime; // Apliquer la gravité
-        controller.Move(velocity * Time.deltaTime);
+        _velocity.y += _gravity * Time.deltaTime; // Apliquer la gravité
+        controller.Move(_velocity * Time.deltaTime);
+    }
+
+    void FixedUpdate()
+    {
+        if (_direction.magnitude >= 0.01f) // test de si ça bouge (tuto : 0.1f)
+        {
+            
+            // On utilise Atan2 pour récupérer l'angle de direction
+            float targetAngle = Mathf.Atan2(_direction.x, _direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y; // angle calculé en fonction de la direction du joueur + la rotation de la cam
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref _turnSmoothVelocity, _turnSmoothTime); // pour smooth l'angle
+            transform.rotation = Quaternion.Euler(0f, angle, 0f); // Appliquer la rotation au joueur
+
+            Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward; // mouvement prenant en compte la rotation de la cam
+            controller.Move(moveDirection.normalized * _speed * Time.deltaTime);
+            
+
+            // New way
+            /*
+            Vector3 direction = transform.forward * _direction.z;
+            _rigidbody.MovePosition(_rigidbody.position + direction * (_speed * Time.fixedDeltaTime));
+
+            Quaternion rightDirection = Quaternion.Euler(0f, _direction.x * (1500f * Time.fixedDeltaTime), 0f);
+            Quaternion newRotation = Quaternion.Slerp(_rigidbody.rotation, _rigidbody.rotation * rightDirection, Time.fixedDeltaTime * 3f); ;
+            _rigidbody.MoveRotation(newRotation);
+            */
+        }
     }
 }
