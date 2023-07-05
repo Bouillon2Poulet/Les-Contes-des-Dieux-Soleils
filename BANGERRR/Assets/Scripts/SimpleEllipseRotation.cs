2085c1bef6eeaa4ec049aa85e028f0bb4951efd7
bouillon2poulet <romain.serres74@gmail.com>
2023-07-05 18:17:09 +0200

    Ignore Eaux Divine files + Add Eaux Divines + Fixed Orbite Jittering + Triton materials

--- BANGERRR/Assets/Scripts/SimpleEllipseRotation.cs
+++ BANGERRR/Assets/Scripts/SimpleEllipseRotation.cs
@@ -3,9 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SimpleEllipseRotation : MonoBehaviour
-{
-
-    
+{   
     public float semiMajorAxis;
     public float semiMinorAxis;
     public float speed = 1;
@@ -19,14 +17,14 @@ public class SimpleEllipseRotation : MonoBehaviour
         transform.position = GetPosition(GetComponentInParent<SystemDayCounter>().systemTime, invertFactor); // Met à jour la position de l'objet sur l'ellipse
     }
     // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
         int invertFactor = (invert) ? -1 : 1;
         transform.position = GetPosition(GetComponentInParent<SystemDayCounter>().systemTime, invertFactor); // Met à jour la position de l'objet sur l'ellipse
     }
 
     Vector3 GetPosition(float time, int invertFactor){
-        float angleBySecond = (speed * 2f * Mathf.PI)/86400f;
+        float angleBySecond = (speed * 2f * Mathf.PI)/86400f; //Nombre secondes dans une journée
         float currentAngle = offset + invertFactor * time * angleBySecond;
         float x = transform.parent.position.x + semiMajorAxis * Mathf.Cos(currentAngle); // Calcul de la coordonnée x sur l'ellipse avec décalage de l'objet parent
         float z = transform.parent.position.z + semiMinorAxis * Mathf.Sin(currentAngle); // Calcul de la coordonnée z sur l'ellipse avec décalage de l'objet parent
